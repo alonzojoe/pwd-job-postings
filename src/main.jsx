65f74fa9f0cbd9871fe569ee7b287783045b3bb3
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-04-16 15:15:20 +0800

    chore: add app assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+//icons
+import "@/assets/vendor/fonts/fontawesome.css";
+import "@/assets/vendor/fonts/tabler-icons.css";
+import "@/assets/vendor/fonts/flag-icons.css";
+
+//core css
+import "@/assets/vendor/css/rtl/core.css";
+import "@/assets/vendor/css/rtl/theme-default.css";
+import "@/assets/vendor/css/demo.css";
+import "@/assets/vendor/fonts/flag-icons.css";
+import "@/assets/vendor/css/rtl/theme-semi-dark.css";
+import "@/assets/css/email.css";
+//pages
+import "@/assets/vendor/css/pages/cards-advance.css";
+import "@/assets/vendor/css/rtl/custom-style.css";
+import "@/assets/vendor/js/bootstrap.js";
+
+//custom
+import "@/assets/css/custom-style.css";
+
+import "sweetalert2/dist/sweetalert2.min.css";
+import "@/assets/vendor/libs/spinkit/spinkit.css";
+import "@/index.css";
+import App from "@/App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
---
+++ src/pages/JobPostings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JobPostings = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default JobPostings
