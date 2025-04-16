50ca7d12c0d777d1b0ba1950c674424e6c26d692
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-04-16 15:22:38 +0800

    feat: setup app router

--- src/pages/JobPostings.jsx
+++ src/pages/JobPostings.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const JobPostings = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <>job postings</>;
+};
 
-export default JobPostings
+export default JobPostings;
