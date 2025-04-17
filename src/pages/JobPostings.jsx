5fe0c47d48574b4cf4ca8997c574bde412ba76fa
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2025-04-17 17:29:26 +0800

    feat: add pagination

--- src/pages/JobPostings.jsx
+++ src/pages/JobPostings.jsx
@@ -251,6 +251,68 @@ const JobPostings = () => {
                       </div>
                     </div>
                   </div>
+                  <div>
+                    <nav aria-label="Page navigation">
+                      <ul className="pagination">
+                        <li className="page-item prev">
+                          <a
+                            className="page-link waves-effect"
+                            href="javascript:void(0);"
+                          >
+                            <i className="ti ti-chevrons-left ti-xs"></i>
+                          </a>
+                        </li>
+                        <li className="page-item active">
+                          <a
+                            className="page-link waves-effect"
+                            href="javascript:void(0);"
+                          >
+                            1
+                          </a>
+                        </li>
+                        <li className="page-item">
+                          <a
+                            className="page-link waves-effect"
+                            href="javascript:void(0);"
+                          >
+                            2
+                          </a>
+                        </li>
+                        <li className="page-item">
+                          <a
+                            className="page-link waves-effect"
+                            href="javascript:void(0);"
+                          >
+                            3
+                          </a>
+                        </li>
+                        <li className="page-item">
+                          <a
+                            className="page-link waves-effect"
+                            href="javascript:void(0);"
+                          >
+                            4
+                          </a>
+                        </li>
+                        <li className="page-item">
+                          <a
+                            className="page-link waves-effect"
+                            href="javascript:void(0);"
+                          >
+                            5
+                          </a>
+                        </li>
+                        <li className="page-item next">
+                          <a
+                            className="page-link waves-effect"
+                            href="javascript:void(0);"
+                          >
+                            <i className="ti ti-chevrons-right ti-xs"></i>
+                          </a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
                 </div>
               </div>
             </div>
