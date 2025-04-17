a02dac4bced6b682fd21b0b729c99a11a59affbc
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2025-04-17 17:49:17 +0800

    style: navbar app logo and text

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Avatar from "@/assets/images/avatar.jpeg";
-
+import MainLogo from "@/assets/images/logo-main.png";
+import MainText from "@/assets/images/logo-text.png";
 const Navbar = () => {
   return (
     <nav
@@ -8,47 +9,20 @@ const Navbar = () => {
       id="layout-navbar"
     >
       <div className="container-xxl">
-        <div className="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+        <div className="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0">
           <div className="navbar-brand app-brand demo d-xl-flex py-0 me-4 d-none d-lg-flex">
             <Link to="/" className="app-brand-link gap-2">
               <span className="app-brand-logo demo">
-                <svg
-                  width="32"
-                  height="22"
-                  viewBox="0 0 32 22"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
-                    fill="#7367F0"
-                  ></path>
-                  <path
-                    opacity="0.06"
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
-                    fill="#161616"
-                  ></path>
-                  <path
-                    opacity="0.06"
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
-                    fill="#161616"
-                  ></path>
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
-                    fill="#7367F0"
-                  ></path>
-                </svg>
+                <img src={MainLogo} width="36" height="30" />
               </span>
               <span className="app-brand-text demo menu-text fw-bold">
-                JobAbility Match
+                <img
+                  src={MainText}
+                  style={{
+                    height: "auto",
+                    width: "150px",
+                  }}
+                />
               </span>
             </Link>
           </div>
